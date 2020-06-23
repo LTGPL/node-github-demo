84c036ee25f94839df9e5dc542e95340ec951753
Leon Giang <leon.giang@invoice2go.com>
2020-06-24 09:40:52 +1000

    update response

--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@ const request = require("supertest");
 const app = require("../index");
 describe("GET /", () => {
   it("respond with Hello World via github actions", (done) => {
-    request(app).get("/").expect("Hello World", done);
+    request(app).get("/").expect("Hello World via github actions", done);
   })
 });
